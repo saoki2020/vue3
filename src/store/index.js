3bd526000f17bbae6ad6cde5cff4825d07cf232e
saoki2020 <md.mendelevium101@gmail.com>
2021-11-25 15:07:31 +0900

    Vue課題3

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,56 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: []
   },
   mutations: {
+    addTask(state, taskInput) {
+      state.tasks.push({
+        id: state.tasks.length,
+        content: taskInput,
+        status: '作業中'
+      })
+    },
+    deleteTask(state, taskId) {
+      state.tasks.splice(taskId, 1)
+    },
+    renumberTask(state) {
+      for(let i = 0; i < state.tasks.length; i++) {
+        state.tasks[i].id = i
+      }
+    },
+    changeStatus(state, taskId) {
+      if (state.tasks[taskId].status === '作業中') {
+        state.tasks[taskId].status = '完了'
+      } else {
+        state.tasks[taskId].status = '作業中'
+      }
+    },
+  },
+  getters: {
+    getAllTasks(state) {
+      return state.tasks
+    },
+    getWorkingTasks(state) {
+      return state.tasks.filter(task => task.status === '作業中')
+    },
+    getDoneTasks(state) {
+      return state.tasks.filter(task => task.status === '完了')
+    },
   },
   actions: {
+    commitTask({commit}, taskInput) {
+      commit('addTask', taskInput)
+    },
+    commitDelete({commit}, taskId) {
+      commit('deleteTask', taskId)
+    },
+    commitRenumber({commit}) {
+      commit('renumberTask')
+    },
+    commitChangeStatus({commit}, taskId) {
+      commit('changeStatus', taskId)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>ToDoリスト</h1>
+    <form>
+      <input type="radio" name="status" id="all" v-model="picked" value="all">
+      <label for="all">すべて</label>
+      <input type="radio" name="status" id="working" v-model="picked" value="working">
+      <label for="working">作業中</label>
+      <input type="radio" name="status" id="done" v-model="picked" value="done">
+      <label for="done">完了</label>
+    </form>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>コメント</th>
+          <th>状態</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks" v-bind:key="task.id">
+          <td>{{ task.id }}</td>
+          <td>{{ task.content }}</td>
+          <td><button @click="changeStatus(task.id)">{{ task.status }}</button></td>
+          <td><button @click="deleteTask(task.id)">削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>新規タスクの追加</h2>
+    <form>
+      <input type="text" name="newTask" v-model="taskInput">
+      <button @click.prevent="addNewTask(taskInput)">追加</button>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      taskInput: '',
+      picked: 'all',
+    }
+  },
+  methods: {
+    addNewTask(taskInput) {
+      this.$store.dispatch('commitTask', taskInput),
+      this.taskInput = ''
+    },
+    changeStatus(taskId) {
+      this.$store.dispatch('commitChangeStatus', taskId)
+    },
+    deleteTask(taskId) {
+      this.$store.dispatch('commitDelete', taskId),
+      this.$store.dispatch('commitRenumber')
+    }
+  },
+  computed: {
+    tasks() {
+      if (this.picked === 'all') {
+        return this.$store.getters.getAllTasks
+      } else if (this.picked === 'working') {
+        return this.$store.getters.getWorkingTasks
+      } else {
+        return this.$store.getters.getDoneTasks
+      }
+    },
   }
 }
 </script>
